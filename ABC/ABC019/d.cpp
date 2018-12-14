d96482eec79050904a4caa60ea6ff45c59853545
suzukishota22 <27115076@stn.nitech.ac.jp>
2018-12-14 21:28:32 +0900

    solve ABC019 D

---
+++ ABC/ABC019/d.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORE(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repe(i, n) for (int i = 0; i <= (n); ++i)
+#define ALL(v) (v).begin(), (v).end()
+#define SP cout << fixed << setprecision(10)
+typedef pair<int, int> P;
+const int inf = (int)1e9;
+const int mod = (int)1e9 + 7;
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+int main(void) {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    int maxdist = 0;
+    int index = 1;
+    FORE(i, 2, n) {
+        cout << "? " << 1 << " " << i << endl;
+        fflush(stdout);
+        int dist;
+        cin >> dist;
+        if (dist > maxdist) {
+            maxdist = dist;
+            index = i;
+        }
+    }
+
+    maxdist = 0;
+    FORE(i, 1, n) {
+        if (i == index) continue;
+        cout << "? " << index << " " << i << endl;
+        fflush(stdout);
+        int dist;
+        cin >> dist;
+        if (dist > maxdist) {
+            maxdist = dist;
+        }
+    }
+
+    cout << "! " << maxdist << endl;
+    fflush(stdout);
+
+    return 0;
+}
