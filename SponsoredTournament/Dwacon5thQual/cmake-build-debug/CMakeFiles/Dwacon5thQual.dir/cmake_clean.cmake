7a6bf84f0f30667d6087ee14f2dd88e04f9c30fd
suzukishota22 <27115076@stn.nitech.ac.jp>
2019-01-15 19:02:59 +0900

    update .gitignore

---
+++ SponsoredTournament/Dwacon5thQual/cmake-build-debug/CMakeFiles/Dwacon5thQual.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dwacon5thQual.dir/b.cpp.o"
+  "Dwacon5thQual.pdb"
+  "Dwacon5thQual"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dwacon5thQual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
