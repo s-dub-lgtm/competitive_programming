36aba9b93ced3e6700f2791e05ce4bcabcae40c5
suzukishota22 <27115076@stn.nitech.ac.jp>
2019-01-25 23:39:12 +0900

    solve AGC001 B

---
+++ AGC/AGC001/b.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+#define FOR(i, a, b) for(int i=(a);i<(b);++i)
+#define FORE(i, a, b) for(int i=(a);i<=(b);++i)
+#define rep(i, n) for(int i=0;i<(n);++i)
+#define repe(i, n) for(int i=0;i<=(n);++i)
+#define ALL(v) (v).begin(),(v).end()
+#define SP cout<<fixed<<setprecision(10)
+typedef pair<int, int> P;
+const int INF = (int) 1e9;
+const int MOD = (int) 1e9 + 7;
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+
+int main(void) {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	
+	ll n, x;
+	cin >> n >> x;
+	
+	cout << (n - gcd(n, x)) * 3 << endl;
+	
+	
+	return 0;
+}
