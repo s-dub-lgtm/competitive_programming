1dc2f9b6e60a66b73b90d52eb1833d6ddfa88619
suzukishota22 <27115076@stn.nitech.ac.jp>
2019-03-25 19:08:26 +0900

    solve AGC067 D

---
+++ ARC/ARC064/d.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORE(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repe(i, n) for (int i = 0; i <= (n); ++i)
+#define ALL(v) (v).begin(), (v).end()
+#define SP cout << fixed << setprecision(10)
+typedef pair<int, int> P;
+const int inf = (int)1e9;
+const int mod = (int)1e9 + 7;
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+int main(void) {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll n, a, b;
+    cin >> n >> a >> b;
+    ll x[n];
+    rep(i, n) cin >> x[i];
+
+    ll sum = 0;
+    rep(i, n - 1) { sum += min((x[i + 1] - x[i]) * a, b); }
+
+    cout << sum << endl;
+
+    return 0;
+}
