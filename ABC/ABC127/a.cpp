8fda091b8f40cae22172048faa94b4ce317c4fb7
suzukishota22 <27115076@stn.nitech.ac.jp>
2019-05-26 00:22:12 +0900

    participate in ABC127

---
+++ ABC/ABC127/a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+#define FOR(i, a, b) for(int i=(a);i<(b);++i)
+#define FORE(i, a, b) for(int i=(a);i<=(b);++i)
+#define FORR(i, a, b) for(int i=(b-1);i>=(a);--i)
+#define FORRE(i, a, b) for(int i=(b);i>=(a);--i)
+#define rep(i, n) for(int i=0;i<(n);++i)
+#define repe(i, n) for(int i=0;i<=(n);++i)
+#define repr(i, n) for(int i=(n-1);i>=0;--i)
+#define repre(i, n) for(int i=(n);i>=0;--i)
+#define ALL(v) (v).begin(),(v).end()
+#define SP cout<<fixed<<setprecision(10)
+typedef pair<int, int> P;
+const int INF = (int) 1e9;
+const int MOD = (int) 1e9 + 7;
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+int main(void) {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	
+	int a,b;
+	cin>>a>>b;
+	
+	if(a>=13) cout<<b<<endl;
+	else if (a >= 6) cout<<b/2<<endl;
+	else cout<<0<<endl;
+	
+	return 0;
+}
