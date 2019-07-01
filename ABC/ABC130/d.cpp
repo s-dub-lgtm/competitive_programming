e64b055c968b53fcbf0e1284a22a640b0f7a129b
suzukishota22 <27115076@stn.nitech.ac.jp>
2019-07-01 23:56:38 +0900

    solve ABCD in ABC130

---
+++ ABC/ABC130/d.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+#define FOR(i, a, b) for(int i=(a);i<(b);++i)
+#define FORE(i, a, b) for(int i=(a);i<=(b);++i)
+#define FORR(i, a, b) for(int i=(b-1);i>=(a);--i)
+#define FORRE(i, a, b) for(int i=(b);i>=(a);--i)
+#define rep(i, n) for(int i=0;i<(n);++i)
+#define repe(i, n) for(int i=0;i<=(n);++i)
+#define repr(i, n) for(int i=(n-1);i>=0;--i)
+#define repre(i, n) for(int i=(n);i>=0;--i)
+#define ALL(v) (v).begin(),(v).end()
+#define SP cout<<fixed<<setprecision(10)
+typedef pair<int, int> P;
+const int INF = (int) 1e9;
+const int MOD = (int) 1e9 + 7;
+
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+int main(void) {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	
+	ll n, k;
+	cin >> n >> k;
+	ll a[n];
+	rep(i, n) cin >> a[i];
+	
+	ll cnt = 0;
+	ll sum = 0;
+	int r = 0;
+	rep(l, n) {
+		while (sum < k) {
+			if (r == n) break;
+			sum += a[r];
+			r++;
+		}
+		if (sum < k) break;
+		cnt += n - r + 1;
+		sum -= a[l];
+	}
+	
+	cout << cnt << endl;
+	
+	return 0;
+}
