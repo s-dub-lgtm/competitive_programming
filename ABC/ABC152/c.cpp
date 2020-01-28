81610c564957cf8401ed52415c3bbcc6c20deb71
s-dub <ckl15076@nitech.jp>
2020-01-28 13:32:02 +0900

    solve ABC in ABC152

---
+++ ABC/ABC152/c.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORE(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repe(i, n) for (int i = 0; i <= (n); ++i)
+#define all(v) (v).begin(), (v).end()
+#define sp cout << fixed << setprecision(10)
+typedef pair<int, int> P;
+const int inf = (int)1e9;
+const int mod = (int)1e9 + 7;
+int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+int main(void) {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    int p[n];
+    rep(i, n) cin >> p[i];
+
+    int cnt = 0;
+    int min = inf;
+    rep(i, n) {
+        if (p[i] < min) {
+            min = p[i];
+            cnt++;
+        }
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
